e2b685b8d66ff3e17ed873ee6bed6832b55b61ae
Jonathan-Chak <133053615+Jonathan-Chak@users.noreply.github.com>
2023-08-01 16:22:38 -0400

    fixes

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -12,7 +12,11 @@ const AUTH_API = 'https://boba-library.onrender.com/api/auth/';
 
 const httpOptions = {
 
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type,Accept'
+    })
 
 };
 

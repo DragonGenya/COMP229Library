a3df10118e9e6b4448e7c48fd7761bbeae43f3de
Jonathan-Chak <133053615+Jonathan-Chak@users.noreply.github.com>
2023-08-02 13:24:04 -0400

    changed port

--- Product/server.js
+++ Product/server.js
@@ -88,7 +88,7 @@ require("./app/routes/category.routes")(app);
 
 // set port, listen for requests
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 
